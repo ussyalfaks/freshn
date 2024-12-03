dc35b5d09502ee2108153601ab01be736344a5c2
ussyalfaks <usmanalfaki@gmail.com>
2024-12-03 20:42:49 +0100

    fixed

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -1,33 +1,26 @@
 "use client"
 
-  import Header from '@/components/Header'
-   import Hero from '@/components/Hero'
-   import PropertyListing from '@/components/PropertyListing'
-   import TopDestination from '@/components/TopDestination'
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import PropertyListing from '@/components/PropertyListing'
+import TopDestination from '@/components/TopDestination'
 import Recommendations from '@/components/Recommendations'
 import Footer from '@/components/footer'
 import Newlistings from "@/components/Newlistings"
+
+
+
+
 export default function Homepage() {
   return (
     <div className='bg-[#f9f9f9] overflow-hidden'>
-     
-     
          <Header /> 
-       
-      
       <main>
         <Hero /> 
          <PropertyListing /> 
          <TopDestination /> 
         <Newlistings />  
         <Recommendations />   
-
-
-        
-
-
-
-      
       </main>
 
        <Footer />
